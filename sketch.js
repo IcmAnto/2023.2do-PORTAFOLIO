8f3de664cc3252ef0cceefa6891846c705c217f5
Antonia.icm <141799753+IcmAnto@users.noreply.github.com>
2023-10-27 03:43:17 -0300

    Update sketch.js correciones canvas
    funcionamiento

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let sStars = [];
+let canvas;
 
 class Star {
   constructor(x, y) {
@@ -48,7 +49,7 @@ function fallDiagonal(element, speed) {
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    canvas = createCanvas(windowWidth, windowHeight);
     canvas.position (0,0);
     canvas.style('z-index','-1');
   
