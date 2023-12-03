ac9b74058ca7e1f2d9d2eecdb5dae6d58bbb06da
Antonia.icm <antonia.campillay.m@mail.pucv.cl>
2023-12-02 23:03:40 -0300

    edificios en css seran

--- menusss/sobreMi/sketch.js
+++ menusss/sobreMi/sketch.js
@@ -6,13 +6,6 @@ function setup() {
     background(215, 154, 37);
     
     noStroke();
-    
-    fill(99, 99, 0);
-    rect(0, height/2, 300, height-150);
-    fill(0, 99, 99);
-    rect(width-400, 25, 400, height-25);
-    fill(0, 130, 130);
-    rect(width/2-350, height-150, 700, 150);
 
     fill(245, 245, 245, 75);
     ellipse(width/2, 130, 80, 130);
@@ -22,3 +15,8 @@ function setup() {
     fill(130, 0, 0);
     triangle(width/2, 0, width/2-130, 130, width/2+130, 130);
   }
+
+  
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
