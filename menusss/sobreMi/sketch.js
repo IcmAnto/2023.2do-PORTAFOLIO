01789cc41fd6855357a9dee513db6c70fd3234bc
Antonia.icm <antonia.campillay.m@mail.pucv.cl>
2023-12-02 22:59:46 -0300

    añadido el fondo de sobreMi

--- menusss/sobreMi/sketch.js
+++ menusss/sobreMi/sketch.js
@@ -1,7 +1,24 @@
 function setup() {
-    createCanvas(windowWidth, 400);
+    createCanvas(windowWidth, windowHeight);
   }
   
   function draw() {
     background(215, 154, 37);
+    
+    noStroke();
+    
+    fill(99, 99, 0);
+    rect(0, height/2, 300, height-150);
+    fill(0, 99, 99);
+    rect(width-400, 25, 400, height-25);
+    fill(0, 130, 130);
+    rect(width/2-350, height-150, 700, 150);
+
+    fill(245, 245, 245, 75);
+    ellipse(width/2, 130, 80, 130);
+    fill(245, 245, 245);
+    ellipse(width/2, 130, 60, 110);
+    
+    fill(130, 0, 0);
+    triangle(width/2, 0, width/2-130, 130, width/2+130, 130);
   }
